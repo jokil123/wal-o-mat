de30a65e3427fae9e8b03f53bdba2b9ae60d8b6f
Georg Niggli <jornig@outlook.com>
2024-09-01 23:08:07 +0200

    quiz changes

---
+++ src/lib/components/quiz.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { fly } from "svelte/transition";
+  const dispatch = createEventDispatcher();
+
+  function handleClick(buttonName: any) {
+    dispatch("buttonclick", { button: buttonName });
+  }
+
+  export let questionNr: number;
+  export let questionNrMax: number;
+  export let question: string;
+  export let answer: "agree" | "neutral" | "disagree" | undefined;
+</script>
+
+<div class="border w-full px-10">
+  <p class="font-semibold text-xl pt-8">Frage {questionNr}/{questionNrMax}</p>
+
+  <p class="text-4xl pt-8">
+    {question}
+  </p>
+  <div class="pb-16 pt-10 font-semibold">
+    <button
+      on:click={() => {
+        answer = "agree";
+        handleClick("agree");
+      }}
+      class="border px-4 py-1 hover:bg-white hover:text-black transition-all ease-in duration-200 mr-2"
+      >stimme zu</button
+    >
+    <button
+      on:click={() => {
+        answer = "neutral";
+        handleClick("neutral");
+      }}
+      class="border px-4 py-1 hover:bg-white hover:text-black transition-all ease-in duration-200 mr-2"
+      >neutral</button
+    >
+    <button
+      on:click={() => {
+        answer = "disagree";
+        handleClick("disagree");
+      }}
+      class="border px-4 py-1 hover:bg-white hover:text-black transition-all ease-in duration-200"
+      >stimme nicht zu</button
+    >
+  </div>
+</div>
--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,4 +1,66 @@
+<script lang="ts">
+  import Quiz from "$lib/components/quiz.svelte";
+
+  let answer: "agree" | "neutral" | "disagree" | undefined;
+  let currentQuestion = 1;
+
+  let pointsVector = [0, 0, 0, 0, 0, 0, 0];
+
+  const questions = [
+    {
+      question: "Ich bevorzuge kältere Klimazonen gegenüber wärmeren.",
+      agree: [0, 0, 0, 0, 0, 0, 0],
+      disagree: [0, 0, 0, 0, 0, 0, 0],
+    },
+    {
+      question: "Ich genieße es, in großen Gruppen von Menschen zu sein.",
+      agree: [0, 0, 0, 0, 0, 0, 0],
+      disagree: [0, 0, 0, 0, 0, 0, 0],
+    },
+    {
+      question: "Ich bin ein starker und selbstbewusster Anführer.",
+      agree: [0, 0, 0, 0, 0, 0, 0],
+      disagree: [0, 0, 0, 0, 0, 0, 0],
+    },
+    {
+      question: "Ich liebe es, regelmäßig lange Strecken zu reisen.",
+      agree: [0, 0, 0, 0, 0, 0, 0],
+      disagree: [0, 0, 0, 0, 0, 0, 0],
+    },
+    {
+      question:
+        "Ich bin für meine Intelligenz und meine Fähigkeit zur Problemlösung bekannt.",
+      agree: [0, 0, 0, 0, 0, 0, 0],
+      disagree: [0, 0, 0, 0, 0, 0, 0],
+    },
+  ];
+
+  function nextQuestion() {
+    console.log(currentQuestion + " " + questions.length);
+
+    if (questions.length > currentQuestion) {
+      currentQuestion += 1;
+    } else {
+      ("Quiz Vorbei");
+    }
+  }
+</script>
+
 <div class="mx-auto max-w-screen-md px-8 flex flex-col justify-start">
   <h1 class="text-8xl font-semibold mt-12">Wal-O-Mat</h1>
   <p class="text-2xl font-semibold mt-3">Atlantis 2024</p>
 </div>
+
+<div class="mt-8 max-w-screen-xl mx-auto">
+  <Quiz
+    question={questions[currentQuestion - 1].question}
+    questionNr={currentQuestion}
+    questionNrMax={questions.length}
+    bind:answer
+    on:buttonclick={() => {
+      nextQuestion();
+    }}
+  />
+</div>
+
+<p>You answerd {answer}</p>
