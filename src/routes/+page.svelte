9809baf562b69f551545e5c599d6748361415344
Georg Niggli <jornig@outlook.com>
2024-09-01 21:48:22 +0200

    mainpage done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<div></div>
+<div class="mx-auto max-w-screen-md px-8 flex flex-col justify-start">
+  <h1 class="text-9xl font-semibold mt-12">Wal-O-Mat</h1>
+  <p class="text-3xl font-semibold mt-3">Atlantis 2024</p>
+
+  <button class=" button px-16 py-3 font-bold text-2xl self-end mt-10"
+    >Start</button
+  >
+
+  <p class="mt-16 text-lg">
+    13 von 14 Walarten, die zur Wahl im Ozean antreten, haben die
+    Wal-o-mat-Thesen beantwortet. Jetzt sind Sie an der Reihe: Vergleichen Sie
+    Ihre Standpunkte mit den Antworten der Walarten.
+  </p>
+  <p class="mt-4">
+    Der Wal-o-mat ist keine Empfehlung, welche Walart Sie wählen sollten,
+    sondern ein Informationsangebot über Wale und ihr Verhalten.
+  </p>
+</div>
+
+<style>
+  .button {
+    border-width: 3px;
+    color: rgb(91, 95, 253);
+  }
+</style>
