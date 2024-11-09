82e704b1d2571f3a9625286b30a5ebf2eb9f7f63
jokil123 <jo.kil.plays@gmail.com>
2024-11-09 17:04:14 +0100

    maybe fix 404 error 🤷‍♂️

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [sveltekit()],
-    test: {
-        globals: true,
-        include: ["src/**/*.{test,spec}.{js,ts}"], // Test file pattern
-        coverage: {
-            reporter: ["text", "html"], // Coverage reports
-        },
-    },
 });
